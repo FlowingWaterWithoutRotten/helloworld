e67de1077e0d83ef247bf6fedb31792f448464ab
shujia <shujia@192.168.2.108>
2019-08-07 08:23:45 +0800

    update

--- src/practice/Bmi.java
+++ src/practice/Bmi.java
@@ -1,9 +1,8 @@
-package helloworld;
+package practice;
 
 import java.util.Scanner;
 
-public class HelloWorld {
-	 
+public class Bmi {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println(" ‰»ÎÃÂ÷ÿ£®kg£©"+"\n");
